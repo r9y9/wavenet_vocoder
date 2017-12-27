e5ebfc725bbcb6f4d29bba113b22d470073716ac
Ryuichi Yamamoto <zryuichi@gmail.com>
2017-12-27 16:04:29 +0900

    init

---
+++ setup.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import setuptools.command.develop
+import setuptools.command.build_py
+import os
+import subprocess
+
+version = '0.0.1'
+
+# Adapted from https://github.com/pytorch/pytorch
+cwd = os.path.dirname(os.path.abspath(__file__))
+if os.getenv('WAVENET_VOCODER_BUILD_VERSION'):
+    version = os.getenv('WAVENET_VOCODER_BUILD_VERSION')
+else:
+    try:
+        sha = subprocess.check_output(
+            ['git', 'rev-parse', 'HEAD'], cwd=cwd).decode('ascii').strip()
+        version += '+' + sha[:7]
+    except subprocess.CalledProcessError:
+        pass
+
+
+class build_py(setuptools.command.build_py.build_py):
+
+    def run(self):
+        self.create_version_file()
+        setuptools.command.build_py.build_py.run(self)
+
+    @staticmethod
+    def create_version_file():
+        global version, cwd
+        print('-- Building version ' + version)
+        version_path = os.path.join(cwd, 'wavenet_vocoder', 'version.py')
+        with open(version_path, 'w') as f:
+            f.write("__version__ = '{}'\n".format(version))
+
+
+class develop(setuptools.command.develop.develop):
+
+    def run(self):
+        build_py.create_version_file()
+        setuptools.command.develop.develop.run(self)
+
+
+setup(name='wavenet_vocoder',
+      version=version,
+      description='PyTorch implementation of WaveNet vocoder',
+      packages=find_packages(),
+      cmdclass={
+          'build_py': build_py,
+          'develop': develop,
+      },
+      install_requires=[
+          "numpy",
+          "scipy",
+          "deepvoice3_pytorch >= 0.0.2",
+      ],
+      extras_require={
+          "train": [
+              "docopt",
+              "tqdm",
+              "tensorboardX",
+              "nnmnkwii >= 0.0.11",
+          ],
+          "test": [
+              "nose",
+          ],
+      })
