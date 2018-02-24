f9c1193fa4e321e5a191542df00221cef9a25621
Ryuichi Yamamoto <zryuichi@gmail.com>
2018-02-24 17:03:16 +0900

    Fix test

--- tests/test_audio.py
+++ tests/test_audio.py
@@ -8,13 +8,13 @@ sys.path.insert(0, join(dirname(__file__), ".."))
 import numpy as np
 from nose.plugins.attrib import attr
 
+import logging
+logging.getLogger('tensorflow').disabled = True
+
 
 @attr("local_only")
 def test_amp_to_db():
-    # this will invoke tensorflow initialization and you will see too many logs
-    # which is annoying. Limit this only for local
     import audio
-
     x = np.random.rand(10)
-    x_hat = audio.db_to_amp(audio._amp_to_db(x))
-    assert (x == x_hat).all()
+    x_hat = audio._db_to_amp(audio._amp_to_db(x))
+    assert np.allclose(x, x_hat)
