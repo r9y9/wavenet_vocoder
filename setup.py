948e1882399d2e5584542c08a9c8bb6d2aac1c50
Ryuichi Yamamoto <zryuichi@gmail.com>
2018-07-08 23:37:30 +0900

    start new dev cycle

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools.command.build_py
 import os
 import subprocess
 
-version = '0.1.0'
+version = '0.1.1'
 
 # Adapted from https://github.com/pytorch/pytorch
 cwd = os.path.dirname(os.path.abspath(__file__))
