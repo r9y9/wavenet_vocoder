99bfe28af8c7552a6e05e01b6bdfdf66502ce541
Ryuichi Yamamoto <zryuichi@gmail.com>
2019-09-07 23:13:36 +0900

    fix

--- wavenet_vocoder/__init__.py
+++ wavenet_vocoder/__init__.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 from __future__ import with_statement, print_function, absolute_import
 
-from .version import __version__
+from .version import version as __version__
 
 from .wavenet import receptive_field_size, WaveNet
